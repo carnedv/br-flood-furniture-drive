647c21ba6d29b096b15bc2d334ae8ecfeeb21dc0
Dave Carner <davec@ashesandwater.com>
2016-08-24 20:38:16 -0500

    Cleaned up `hybrid-auth` config file.

--- app/config/hybrid-auth.php
+++ app/config/hybrid-auth.php
@@ -18,18 +18,8 @@ return [
                     "id" => "73809102130-8sd6h9920ff2jp17frp3mo3hiacocmqh.apps.googleusercontent.com",
                     "secret" => "SNg8RHcHkMQFeb2oSpsppqHN"
                 ),
-            )/*,
-            "Facebook" => array(
-                "enabled" => true,
-                "keys" => array("id" => "", "secret" => ""),
-                "trustForwarded" => false
-            ),*/
+            )
         ),
-        // If you want to enable logging, set 'debug_mode' to true.
-        // You can also set it to
-        // - "error" To log only error messages. Useful in production
-        // - "info" To log info and error messages (ignore debug messages)
         "debug_mode" => false,
-        // Path to file writable by the web server. Required if 'debug_mode' is not false
         "debug_file" => "../logs/hybrid-auth.log"
 ];
